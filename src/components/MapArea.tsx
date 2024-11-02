94a620f1ee0bd4afaaab246f0900fbcbaa2c63aa
Ian Mayo <ian@planetmayo.com>
2024-11-02 18:59:35 +0000

    bounds of map should be buffer around current data area

--- src/components/MapArea.tsx
+++ src/components/MapArea.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { AttributionControl, MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapArea.css';
 import { FeatureCollection } from 'geojson';
 import State from '../state';
+import * as L from 'leaflet'
 
 interface MapAreaProps {
   state: State;
@@ -11,10 +12,20 @@ interface MapAreaProps {
 
 const MapArea: React.FC<MapAreaProps> = ({ state }) => {
   const currentState: FeatureCollection = state.getState();
+  const mapRef = useRef<L.Map>(null);
+
+  useEffect(() => {
+    if (mapRef.current !== null) {
+      const map = mapRef.current;
+      const bounds = new L.GeoJSON(currentState).getBounds();
+      const bufferedBounds = bounds.pad(1); // 1km buffer
+      map.fitBounds(bufferedBounds);
+    }
+  }, [currentState]);
 
   return (
     <div className="map-area">
-      <MapContainer bounds={[[51.505, -0.09], [51.505, -0.09]]} style={{ height: "100%", width: "100%" }}>
+      <MapContainer ref={mapRef} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
