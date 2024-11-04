f9e68ef40422a41aad77b142fdfd21b087c9a977
Ian Mayo <ian@planetmayo.com>
2024-11-04 11:21:29 +0000

    Refactor how line is regenerated

--- src/components/MapArea.tsx
+++ src/components/MapArea.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { AttributionControl, MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapArea.css';
-import { FeatureCollection } from 'geojson';
+import { FeatureCollection, Point } from 'geojson';
 import * as L from 'leaflet'
 import { printFeature } from '../state';
 import { buffer, lineString, featureCollection } from '@turf/turf';
@@ -12,34 +12,47 @@ interface MapAreaProps {
 }
 
 const convertPointsToLine = (points: FeatureCollection) => {
-  const coordinates = points.features.map(feature => feature.geometry.coordinates);
-  const line = lineString(coordinates);
-  return featureCollection([line]);
+  if (points.type === 'FeatureCollection') {
+    if(points.features.length > 0) {
+      if (points.features[0].geometry.type === 'Point') {
+        const coordinates = points.features.map(feature => {
+          const point: Point = feature.geometry as Point
+          return point.coordinates
+        });
+        const line = lineString(coordinates);
+        return featureCollection([line]);
+      }
+    }
+  }
+  return points;
 };
 
 const MapArea: React.FC<MapAreaProps> = ({ state }) => {
   const mapRef = useRef<L.Map>(null);
   const [bounds, setBounds] = useState<L.LatLngBounds | null>(null);
   const [mousePosition, setMousePosition] = useState<{ lat: number; lng: number } | null>(null);
-
+  const [renderedState, setRenderedState] = useState<FeatureCollection | null>(null);
+  
   useEffect(() => {
     if (mapRef.current !== null && !bounds && state) {
       const map = mapRef.current;
-      const bufferedState: FeatureCollection = buffer(state, 500, { units: 'kilometers' });
+      const bufferedState: FeatureCollection = buffer(state, 50, { units: 'kilometers' });
       const bounds = new L.GeoJSON(bufferedState).getBounds();
       setBounds(bounds.pad(5));
       console.log('fitting bounds', state?.features[0].geometry, bounds.getNorthWest(), bounds.getSouthEast());
       map.fitBounds(bounds);
     }
   }, [state, bounds, setBounds]);
-
+  
   useEffect(() => {
     if (state) {
       printFeature('map update', state);
+      
+      setRenderedState(convertPointsToLine(state));
     }
   }, [state]);
-
-
+  
+  
   useEffect(() => {
     const map = mapRef.current;
     if (map) {
@@ -53,48 +66,39 @@ const MapArea: React.FC<MapAreaProps> = ({ state }) => {
       };
     }
   }, [mapRef]);
-
-  if (state === null) {
+  
+  if (renderedState === null) {
     return <div>Loading...</div>;
   } else {
-    const customRenderer = (featureCollection: FeatureCollection) => {
-      if (featureCollection.name === 'Route') {
-        return convertPointsToLine(featureCollection);
-      }
-      return featureCollection;
-    };
-
-    const renderedState = customRenderer(state);
-
     return (
       <div className="map-area" style={{ position: 'relative' }}>
-        <MapContainer ref={mapRef} style={{ height: "100%", width: "100%" }}>
-          <TileLayer
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          />
-          <GeoJSON key={JSON.stringify(renderedState)} data={renderedState} />
-          <AttributionControl position="bottomright" />
-        </MapContainer>
-        {mousePosition && (
-          <div
-            className="mouse-tracker"
-            style={{
-              position: 'absolute',
-              bottom: 10,
-              left: 10,
-              background: 'rgba(255, 255, 255, 0.8)',
-              padding: '5px',
-              borderRadius: '4px',
-              zIndex: 1000,
-            }}
-          >
-            Lat: {mousePosition.lat.toFixed(2)}, Lng: {mousePosition.lng.toFixed(2)}
-          </div>
-        )}
+      <MapContainer ref={mapRef} style={{ height: "100%", width: "100%" }}>
+      <TileLayer
+      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <GeoJSON key={JSON.stringify(renderedState)} data={renderedState} />
+      <AttributionControl position="bottomright" />
+      </MapContainer>
+      {mousePosition && (
+        <div
+        className="mouse-tracker"
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          left: 10,
+          background: 'rgba(255, 255, 255, 0.8)',
+          padding: '5px',
+          borderRadius: '4px',
+          zIndex: 1000,
+        }}
+        >
+        Lat: {mousePosition.lat.toFixed(2)}, Lng: {mousePosition.lng.toFixed(2)}
+        </div>
+      )}
       </div>
     );
   }
-
+  
 }
 
 export default MapArea;
